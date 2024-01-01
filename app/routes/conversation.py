fcc35ed6e3c6301d6e2327b850d99845ae950aa9
Kelvin Ward <kelvinward1010@gmail.com>
2024-01-01 22:05:05 +0700

    update conversation id

--- app/routes/conversation.py
+++ app/routes/conversation.py
@@ -55,6 +55,28 @@ async def get_all_conversations_token(current_user = Depends(oauth2.get_current_
     
     return {"data": all_in_conversations}
 
+@router.get("/conversation_id/{id}", status_code=status.HTTP_202_ACCEPTED)
+async def get_conversations_id(id):
+    conversation_find = schemas.initial_conversation(database.collection_conversations.find_one({"_id": ObjectId(id)}))
+    
+    if not conversation_find:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Not found conversation with id: {id}")
+    
+    user_in_conversation = dict(id = conversation_find['id'], 
+                                user_1 = schemas.initial_user(database.collection_users.find_one({"_id": ObjectId(conversation_find['user_1'])})),
+                                user_2 = schemas.initial_user(database.collection_users.find_one({"_id": ObjectId(conversation_find['user_2'])})),
+                                created_at = conversation_find['created_at'])
+    messages_find = schemas.list_messages(database.collection_messages.find({"conversation_id": conversation_find['id']}))
+    messages_in_conversation = list(dict(message, user = schemas.initial_user(database.collection_users.find_one({"_id": ObjectId(message['owner_id'])}))) for message in messages_find)
+    
+    all_in_conversation = dict(user_in_conversation, 
+                                messages = messages_in_conversation,
+                            )
+    
+    
+    return {"data": all_in_conversation}
+    
+
 
 @router.post("/create", status_code=status.HTTP_201_CREATED)
 async def create_conversation(conversation: models.Conversation, current_user = Depends(oauth2.get_current_user)):
